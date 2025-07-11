b51ab947ea658c8150174380466bde863dc694dd
adrianhvakd <adrianhvakd@gmail.com>
2025-07-11 18:07:23 -0400

    practica 11-07-2025

---
+++ src/cliente/cliente.controller.ts
@@ -0,0 +1,31 @@
+import { Controller, Get, Post, Render } from '@nestjs/common';
+import { ClienteService } from './cliente.service';
+
+@Controller('cliente')
+export class ClienteController {
+    constructor(private clienteService:ClienteService){}
+    //listar
+    @Get('')
+    @Render('cliente/index')
+    async index(){
+        let items = await this.clienteService.getAll();
+        return {items:items}
+    }
+    //crear -form
+    @Get('/formcliente')
+    @Render('cliente/form')
+    form(){}
+    //crear -storage
+    @Post('')
+    create(){}
+    //crear -editar
+    @Get('/formclienteedit')
+    @Render('cliente/form')
+    formEdit(){}
+    //crear -storage
+    @Post('/edit/:id')
+    update(){}
+    //delete
+    @Get('/delete/:id')
+    delete(){}
+}
---
+++ src/cliente/cliente.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { ClienteEntity } from './entities/cliente.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class ClienteService {
+
+    constructor(@InjectRepository(ClienteEntity)
+    private ClienteRepository: Repository<ClienteEntity>,)
+    {}
+
+    public async getAll(){
+        return await this.ClienteRepository.find();
+    }
+    public async create(cliente:ClienteEntity){
+        let clienteDb= await this.ClienteRepository.create(cliente);
+        return clienteDb;
+    }
+    public async edit(cliente,id){
+        let respuesta = await this.ClienteRepository.update({id:id},cliente);
+        return respuesta.affected??0>=1?true:false
+
+    }
+    public async delete(id){
+        let respuesta = await this.ClienteRepository.delete(id);
+        return respuesta.affected??0>=1?true:false
+    }
+    public async getById(id) {
+        return this.ClienteRepository.findOne(id);
+    }
+
+
+
+}
