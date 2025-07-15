ad14f2c91ff0396b470f2ea4929161e748905813
Adrianhvakd <adrianhvakd@gmail.com>
2025-07-15 12:58:07 -0500

    Modificacion con la ultima tarea

--- src/cliente/dtos/clienteNuevo.dto.ts
+++ src/cliente/dtos/clienteNuevo.dto.ts
@@ -1,22 +1,19 @@
-import { IsInt, IsPositive, Length, Max, Min } from "class-validator";
+import { IsInt, IsPositive, Length, Matches, Max, Min } from "class-validator";
 
 export class ClienteNuevoDto{
-    @Min(10)
-    @Max(12)
-    @IsInt()
-    @IsPositive()
+
+    @Length(7,12, { message: "El ci: Debe tener un tamaño entre 7-12 digitos"})
+    @Matches(/^[1-9][0-9]*$/, { message: "El ci: Debe contener solo números y no empezar con 0" })
     ci:string;
 
-    @Length(3,30)
+    @Length(3,30, { message: "El nombre: Debe tener un tamaño entre 3-30 caracteres"})
     nombre:string;
 
-    @Length(3,30)
+    @Length(3,30, { message: "Los apelldios: Deben tener un tamaño entre 3-30 caracteres"})
     apellidos:string;
 
-    @Min(7)
-    @Max(8)
-    @IsInt()
-    @IsPositive()
-    cellular:number;
+    @Length(7,8, { message: "El celular: Debe tener un tamaño entre 7-8 digitos"})
+    @Matches(/^[1-9][0-9]*$/, { message: "El celular: Debe contener solo números y no empezar con 0" })
+    cel:string;
 
 }
