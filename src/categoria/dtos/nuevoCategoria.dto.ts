d17da96ef2b03fb389128488b0c9f203c3f7bfa7
adrianhvakd <adrianhvakd@gmail.com>
2025-07-16 17:48:58 -0400

    next

--- src/categoria/dtos/nuevoCategoria.dto.ts
+++ src/categoria/dtos/nuevoCategoria.dto.ts
@@ -3,7 +3,7 @@ import { Length } from "class-validator";
 export class nuevoCategoriadto{
     @Length(3,50,{message: "Nombre: El nombre debe estar en un rango de 3-50 caracteres"})
     nombre:string;
-
+    @Length(3,50,{message: "Foto: La foto debe estar en un rango de 3-50 caracteres"})
     foto:string;
 
 }
--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,23 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import * as hbs from 'express-handlebars';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.engine('hbs',hbs({
+    extname: 'hbs',
+    partialsDir: join(__dirname,'..','views/partials')
+  }));
+  app.setViewEngine('hbs');
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       transform: true,
     }),
   );
-  app.useStaticAssets(join(__dirname, '..', 'public'));
-  app.setBaseViewsDir(join(__dirname, '..', 'views'));
-  app.setViewEngine('hbs');
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
